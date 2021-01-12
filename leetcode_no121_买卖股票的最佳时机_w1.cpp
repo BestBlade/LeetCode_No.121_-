e878988ca4a9600d8e346b9f538eb8cf988f87a4
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-01-12 11:02:42 +0800

    Add files via upload

---
+++ leetcode_no121_买卖股票的最佳时机_w1.cpp
@@ -0,0 +1,86 @@
+﻿/* ------------------------------------------------------------------|
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+
+如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+
+注意：你不能在买入股票前卖出股票。
+
+ 
+
+示例 1:
+
+输入: [7,1,5,3,6,4]
+输出: 5
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+示例 2:
+
+输入: [7,6,4,3,1]
+输出: 0
+解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*
+*
+*	执行用时：12 ms, 在所有 C++ 提交中击败了67.64%的用户
+*	内存消耗：12.9 MB, 在所有 C++ 提交中击败了71.77%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+int maxProfit(vector<int>& prices) {
+    int minval = INT_MAX;
+    int res = 0;
+    for (int& price : prices) {
+        minval = min(price, minval);
+        res = max(res, price - minval);
+    }
+    return res;
+}
+
+int main() {
+    vector<int> prices = { 7,1,5,3,6,4 };
+    int res = maxProfit(prices);
+}
